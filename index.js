50bca22a21fdb100cfbe6b3c3e2c9ba11995c34a
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-11-25 18:32:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget(){
 }
 
 function deepestChild(){
-  var element = document.querySelectorAll('div#grand-node div div div div')
+  var element = document.getElementById('grand-node').innerHTML
   return element
 }
