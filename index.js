2754172245f4d6adb80cc299f285a31dfd6101e7
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-11-25 19:24:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector){
+  var element = document.querySelector(selector)
+  return element
+}
 
+function nestedTarget(){
+  var element = document.querySelector('.target')
+  return element
+}
+
+function deepestChild(){
+  var element = document.getElementById('grand-node')
+                .querySelector('div div div div div')
+  return element
+}
+
+function increaseRankBy(n){
+  var element = document
+                .getElementById('app')
+                .querySelectorAll('ul.ranked-list li');
+
+  for(var i = 0; i < element.length; i++){
+    element[i].innerHTML = (parseInt(element[i].innerHTML, 10) + n).toString()
+  }
+  return element
+}
